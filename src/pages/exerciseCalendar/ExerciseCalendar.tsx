be3423fcb93f056d2b2ee0ecdd864817e32f1b9d
Adam <adam.tyran@gmail.com>
2023-10-20 19:32:43 +0200

    Added correct format od date in calendar

--- src/pages/exerciseCalendar/ExerciseCalendar.tsx
+++ src/pages/exerciseCalendar/ExerciseCalendar.tsx
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 
 const ExerciseCalendar = () => {
+  const [date, setDate] = useState<Object | undefined>(
+    new Date().toLocaleDateString()
+  );
+
   const fetchExerciseDay = (value: any) => {
     console.log(value);
     console.log(typeof value);
   };
 
   return (
-    <div className=" max-w-md px-2 pt-10 mx-auto">
+    <div className="px-2 pt-10 mx-auto w-screen flex justify-center">
       <Calendar
-        className="text-black h-1/2 w-1/2"
-        onClickDay={(value) => fetchExerciseDay(value)}
+        className="text-black h-1/2 w-max"
+        // onClickDay={(value) => fetchExerciseDay(value)}
+        onClickDay={(value) => setDate(value.toLocaleDateString())}
       />
+      <div>{date && date.toString()}</div>
     </div>
   );
 };
