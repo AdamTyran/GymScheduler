d5fcd6a3b850f85432774ab69d834271a33cc040
Adam <adam.tyran@gmail.com>
2023-10-22 20:52:15 +0200

    Added useAxiosFetch

---
+++ src/hooks/useAxiosFetch.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { ExerciseList } from "../pages/mainPage/MainPage";
+
+const useAxiosFetch = <T>(dataUrl: string, initValue?: T) => {
+  const [data, setData] = useState<T | undefined>(initValue);
+
+  useEffect(() => {
+    const func = async () => {
+      try {
+        const response = await axios.get<T>(dataUrl);
+        setData(response.data);
+        console.log(response.data);
+      } catch (err: any) {
+        console.log(err.message);
+      }
+    };
+    func();
+  }, [dataUrl]);
+
+  return { data };
+};
+
+export default useAxiosFetch;
--- src/pages/exerciseCalendar/ExerciseCalendar.tsx
+++ src/pages/exerciseCalendar/ExerciseCalendar.tsx
@@ -1,22 +1,26 @@
 import React, { useState } from "react";
 import Calendar from "react-calendar";
 import "./Calendar.css";
+import useAxiosFetch from "../../hooks/useAxiosFetch";
+import { ExerciseList } from "../mainPage/MainPage";
 
 const ExerciseCalendar = () => {
+  const url = "http://localhost:3500/exercises";
   const [date, setDate] = useState<Object | undefined>(
     new Date().toLocaleDateString()
   );
+  const { data } = useAxiosFetch<ExerciseList[]>(url);
 
-  const fetchExerciseDay = (value: any) => {
-    console.log(value);
-    console.log(typeof value);
+  const log = () => {
+    console.log(data);
+    data?.map((item) => console.log(item));
   };
 
   return (
     <div className="px-2 pt-10 mx-auto w-screen flex justify-center">
       <Calendar
-        // onClickDay={(value) => fetchExerciseDay(value)}
-        onClickDay={(value) => setDate(value.toLocaleDateString())}
+        // onClickDay={(value) => setDate(value.toLocaleDateString())}
+        onClickDay={log}
       />
       {/* <div>{date && date.toString()}</div> */}
     </div>
