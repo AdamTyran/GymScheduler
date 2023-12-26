a4d7c3a79c344c77ca57f5990dd3de6dca0bf8d1
Adam Tyran <adam.tyran@gmail.com>
2023-12-26 09:56:53 +0100

    Added connection to mongodb

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+const mongodbUri =
+  "mongodb+srv://root:MU3mJtH3QSQlTZ28@gymscheduler.c8rayg3.mongodb.net/";
 
-app.listen(3000, () => {
-  console.log("hello world");
-});
+mongoose
+  .connect(mongodbUri)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("connected to database and server");
+    });
+  })
+  .catch(() => console.log("error"));
