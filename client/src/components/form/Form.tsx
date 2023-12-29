8a2f98b7093b594327c872a4ab670364d061a9da
Adam Tyran <adam.tyran@gmail.com>
2023-12-29 16:45:32 +0100

    Changed names and map keys

--- client/src/components/form/Form.tsx
+++ client/src/components/form/Form.tsx
@@ -4,23 +4,23 @@ import FormAdd from "../FormAdd/FormAdd";
 interface Props {
   setter(args: ExerciseSet): void;
   clickedDate: Date;
-  newExerciseDay: Exercise[];
-  setNewExerciseDay(args: Exercise[]): void;
+  newExerciseDaySet: Exercise[];
+  setNewExerciseDaySet(args: Exercise[]): void;
 }
 
 const Form = ({
   setter,
   clickedDate,
-  newExerciseDay,
-  setNewExerciseDay,
+  newExerciseDaySet,
+  setNewExerciseDaySet,
 }: Props) => {
   return (
     <form onSubmit={(e) => e.preventDefault()} className="w-full px-2 mx-auto">
       <div className="flex flex-col gap-4">
         <FormAdd
           setter={setter}
-          exerciseList={newExerciseDay}
-          setExerciseList={setNewExerciseDay}
+          exerciseList={newExerciseDaySet}
+          setExerciseList={setNewExerciseDaySet}
           clickedDate={clickedDate}
         />
       </div>
